6832e3043eec9d7962980de7d0082904711168f5
Hunter <Huntermatyi@gmail.com>
2024-02-28 12:46:39 -0500

    donezo

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,181 @@
 class Article:
+    all = []
     def __init__(self, author, magazine, title):
-        self.author = author
+        if not isinstance(title, str) or not 5<= len(title) <= 50:
+            raise ValueError("Title must be a string between 5 and 50 characters")
+        if not isinstance(author, Author):
+            raise TypeError("author must be of type Author")
+        self._author = author
         self.magazine = magazine
-        self.title = title
+        self._title = title
+        self._author.add_articles(self)
+        self._magazine.add_articles(self)
+        Article.all.append(self)
+    
+    
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, new_author):
+        if not isinstance(new_author, Author):
+            raise TypeError("author must be of type Author")
+        else:
+            self._author = new_author
+    
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if not isinstance(new_magazine, Magazine):
+            raise TypeError("magazine must be of type Magazine")
+        else:
+            self._magazine = new_magazine
         
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Name must be a string with at least 1 character")
+        self._name = name
+        self._articles = []
+    
+    @property  
+    def name(self):
+        return self._name
 
     def articles(self):
-        pass
+        return self._articles
+    
+    def add_articles(self, article):
+        if isinstance(article, Article):
+            self._articles.append(article)
+        else:
+            raise TypeError("Must be of type Article")
+
 
     def magazines(self):
-        pass
+     unique_magazines_set = {article.magazine for article in self._articles}
+     return list(unique_magazines_set)
+
+
+
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        if not self._articles:
+            return None
+        unique_categories_set = {article.magazine.category for article in self._articles}
+        return list(unique_categories_set)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self._articles = []
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str):
+            if 2 <= len(new_name) <=16:
+                self._name = new_name
+            else: 
+                raise ValueError("Name must be between 2 and 16 characters")
+        else:
+            raise TypeError("Name must be a string")
+    
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, new_category):
+        if isinstance(new_category, str):
+            if len(new_category) > 0:
+                self._category = new_category
+            else:
+                raise ValueError("Category must be more than 0 characters")
+        else:
+            raise TypeError("Name must be a string")
+        
+        
+    def add_articles(self, article):
+        if isinstance(article, Article):
+            self._articles.append(article)
+        else:
+            raise TypeError("Must be of type Article")
+
 
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
+        unique_authors_set = {article.author for article in self._articles}
+        return list(unique_authors_set) if unique_authors_set else None
+
 
     def article_titles(self):
-        pass
+        if not self._articles:
+            return None
+        unique_categories = list()
+        for article in self._articles:
+            if article.magazine:
+                unique_categories.append(article.title)
+        return list(unique_categories)
+
 
     def contributing_authors(self):
-        pass
+        author_counts = {}
+        for article in self._articles:
+            author = article.author
+            if author in author_counts:
+                author_counts[author] += 1
+            else: 
+                author_counts[author] = 1
+        
+        double_authors = [author for author, count in author_counts.items() if count > 2]
+
+        if double_authors:
+            return double_authors
+        else:
+            return None
